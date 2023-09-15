ca5630007b0181417cd0b93e9f1b9a8b42a226d0
stepienbartosz <stepienbartoszzsi@gmail.com>
2023-09-15 09:57:04 +0200

    php 1

---
+++ php 1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index.php</title>
+</head>
+<body>
+    <?php echo 'Tu funkcja echo' ?> <br>
+    <?php echo 'TU' . ' funkcja' . ' echo' ?> <br>
+    <?php echo "TU" . " funkcja" . " echo" ?> <br>
+    <?php echo ('TU' . ' funkcja' . ' echo') ?> <br>
+    <br>
+    <br>
+    <?php print ('Zgłasza ' . 'sie ' . 'funkca ' . 'print') ?> <br>
+    <?php print 'Zgłasza ' . 'sie ' . 'funkca ' . 'print' ?>
+    <br>
+    <br>
+    <?= 'Zgłasza ' . 'sie ' . 'funkca ' . 'print' ?> <br>
+    <?= ('Zgłasza ' . 'sie ' . 'funkca ' . 'print') ?> <br>
+    <br>
+    <br>
+        <?php // komentarz 1 
+        /* komentarz 2
+        */
+        ?>
+
+    
+</body>
+</html>
