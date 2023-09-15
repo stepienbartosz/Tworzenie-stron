500d2ebc72457c06985061e4ef7afc5350553065
stepienbartosz <stepienbartoszzsi@gmail.com>
2023-09-15 10:25:31 +0200

    zmienne

--- php 1.php
+++ php 1.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>index.php</title>
 </head>
-<body>
+<body>pan wesołek
     <?php echo 'Tu funkcja echo' ?> <br>
     <?php echo 'TU' . ' funkcja' . ' echo' ?> <br>
     <?php echo "TU" . " funkcja" . " echo" ?> <br>
